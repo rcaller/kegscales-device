4b613b6997dc3f045a891babd0d8d761f7fee02c
rcaller <rcaller@email.com>
2024-11-07 14:37:40 +0000

    V0.1 update Readme

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-Micropython device code targetting RPi Pico designed as part of KegScales project
+# Kegscales - device
+
+## Summary
+
+Micropython for the RaspBerry Pi Pico Kegscales project
+
+## Project Outline
+The kegscales project is attempting to create a replacement for the soon to be defunct Plaato Keg.
+The project aims to create a device that can measure how much beverage remains in a keg using easily 
+available components and a 3d printed enclosure.
+
+The project currently consists of the Raspberry Pi Pico communicating over BLE with an android app
+
+## This Repository
+This repository contains micropython code to be installed onto a Raspberry Pi Pico W microcontroller.
+
+### Installation
+First you need to download the micropython runtime from here 
+  
+https://www.raspberrypi.com/documentation/microcontrollers/micropython.html
+
+Then use your preferred micropython dev system (Thonny, PyCharm etc) to push the code onto the board.
--- logserver.py
+++ logserver.py
@@ -10,34 +10,35 @@ s = socket.socket()
 class LogServer:
     def __init__(self):
         self.conn=""
-        wlan = network.WLAN(network.STA_IF)
-        wlan.active(True)
-        network.hostname("kegscales")
-        wlan.connect(ssid, password)
-        # Wait for Wi-Fi connection
-        connection_timeout = 10
-        while connection_timeout > 0:
-            if wlan.status() >= 3:
-                break
-            connection_timeout -= 1
-            print('Waiting for Wi-Fi connection...')
-            time.sleep(1)
-
-        # Check if connection is successful
-        if wlan.status() != 3:
-            raise RuntimeError('Failed to establish a network connection')
-        else:
-            print('Connection successful!')
+        if ssid!="":
+            wlan = network.WLAN(network.STA_IF)
+            wlan.active(True)
             network.hostname("kegscales")
-            network_info = wlan.ifconfig()
-            print('IP address:', network_info[0])
-        addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.bind(addr)
-        s.listen()
+            wlan.connect(ssid, password)
+            # Wait for Wi-Fi connection
+            connection_timeout = 10
+            while connection_timeout > 0:
+                if wlan.status() >= 3:
+                    break
+                connection_timeout -= 1
+                print('Waiting for Wi-Fi connection...')
+                time.sleep(1)
+
+            # Check if connection is successful
+            if wlan.status() != 3:
+                raise RuntimeError('Failed to establish a network connection')
+            else:
+                print('Connection successful!')
+                network_info = wlan.ifconfig()
+                print('IP address:', network_info[0])
+            addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            s.setblocking(False)
+            s.bind(addr)
+            s.listen()
 
     def connect(self):
-        if self.conn == "":
+        if ssid!="" and self.conn == "":
             try:
                 self.conn, addr = s.accept()
                 print('Got a connection from', addr)
@@ -49,11 +50,12 @@ class LogServer:
                 self.conn.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
                 self.conn.send("<head></head><body><h1>Kegscales Log</h1><p>")
 
-
             except OSError as e:
-                self.conn.close()
-                print('Connection closed')
-        print("Conn Attempt")
+                if(e.errno == 11):
+                    print("No debug connected")
+                else:
+                    print('Connection error ' + str(e))
+
 
 
     def log(self, logline):
